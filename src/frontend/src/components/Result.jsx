7224250b5ad60ee710b4cb007d6c11b4c27be857
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-10-12 15:58:14 +0300

    Display rounded percent in result

--- src/frontend/src/components/Result.jsx
+++ src/frontend/src/components/Result.jsx
@@ -1,11 +1,14 @@
 const Result = ({item}) => {
+  let percent = item["prosentti"]*100
+  percent = percent - percent%1
+
   return(
     <div className="result">
       <h3>{item["hakukohde"]}</h3>
       <p>Region - Maakunta: {item["maakuntaHakukohde"]}</p>
       <p>City - Kaupunki: {item["kuntaHakukohde"]}</p>
       <p>University - Korkeakoulu: {item["korkeakoulu"]}</p>
-      <p>Intake percentage - Sisäänottoprosentti: {item["prosentti"]*100}%</p>
+      <p>Intake percentage - Sisäänottoprosentti: {percent}%</p>
 
     </div>
   )
